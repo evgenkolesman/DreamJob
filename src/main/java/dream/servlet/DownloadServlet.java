2528a3c8f7f78d0aa422b81971fdbc0eef01b835
evgenkolesman <glavstroi_e@mail.ru>
2021-08-30 00:40:21 +0300

    Не работает скачивание

---
+++ src/main/java/dream/servlet/DownloadServlet.java
@@ -0,0 +1,29 @@
+package dream.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class DownloadServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        File users = null;
+        for (File file : new File("/Documents/").listFiles()) {
+            if (".txt".equals(file.getName())) {
+                users = file;
+                break;
+            }
+        }
+        try (FileInputStream stream = new FileInputStream(users)){
+            resp.getOutputStream().write(stream.readAllBytes());
+        }
+        resp.setContentType("application/octet-stream");
+        resp.setHeader("Content-Disposition", "attachment; filename=\"" + users.getName() + "\"");
+    }
+
+}
