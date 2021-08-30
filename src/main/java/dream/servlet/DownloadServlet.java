7416e6b4ca5fefa33ddeb183366144a4f9c9fa19
evgenkolesman <glavstroi_e@mail.ru>
2021-08-30 13:05:16 +0300

    Не работает скачивание

--- src/main/java/dream/servlet/DownloadServlet.java
+++ src/main/java/dream/servlet/DownloadServlet.java
@@ -7,23 +7,26 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Objects;
 
 public class DownloadServlet extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        File users = null;
-        for (File file : new File(":/IdeaProjects/DreamJob/").listFiles()) {
-            if ("User.txt".equals(file.getName())) {
-                users = file;
-                break;
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            String name = req.getParameter("name");
+            File downloadFile = null;
+            for (File file : Objects.requireNonNull(new File("./Downloads/").listFiles())) {
+                if (name.equals(file.getName())) {
+                    downloadFile = file;
+                    break;
+                }
+            }
+            resp.setContentType("application/octet-stream");
+            resp.setHeader("Content-Disposition", "attachment; filename=\"" + downloadFile.getName() + "\"");
+            try (FileInputStream stream = new FileInputStream(downloadFile)){
+                resp.getOutputStream().write(stream.readAllBytes());
             }
         }
-        try (FileInputStream stream = new FileInputStream(users)){
-            resp.getOutputStream().write(stream.readAllBytes());
-        }
-        resp.setContentType("application/octet-stream");
-        resp.setHeader("Content-Disposition", "attachment; filename=\"" + users.getName() + "\"");
     }
 
-}
+
