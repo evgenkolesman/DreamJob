aa889afca33aac23dbd22d05cae7e219d7d3e6f7
evgenkolesman <glavstroi_e@mail.ru>
2021-08-31 19:58:05 +0300

    0. Страница Login.jsp [#281230]

--- src/main/java/dream/model/User.java
+++ src/main/java/dream/model/User.java
@@ -1,2 +1,58 @@
-package dream.model;public class User {
+package dream.model;
+
+import java.util.Objects;
+
+public class User implements Model {
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
