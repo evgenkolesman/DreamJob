93556d01ce5b172c825f2489006b351700988805
evgenkolesman <glavstroi_e@mail.ru>
2021-09-07 19:46:29 +0300

    4.1. JSON формат. [#238554]

---
+++ src/main/java/dream/model/Email.java
@@ -0,0 +1,15 @@
+package dream.model;
+
+public class Email{
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
